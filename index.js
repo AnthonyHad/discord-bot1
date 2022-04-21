6e35521412282b79b1c8977204d4ec507b42ea51
Anthony Hadwan <anthony.hadwan@hec.edu>
2022-04-22 00:41:48 +0200

    added some better file hierarchy

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// attaching or client to commands allows us to access commands in other folders
+client.commands = new Collection();
+
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -15,19 +29,29 @@ client.once('ready', () => {
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  const { commandName } = interaction;
+  const command = client.commands.get(interaction.commandName);
 
-  if (commandName === 'ping') {
-    await interaction.reply('Pong!');
-  } else if (commandName === 'server') {
-    await interaction.reply(
-      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-    );
-  } else if (commandName === 'user') {
-    await interaction.reply(
-      `Your Tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
-    );
+  try {
+    await command.execute(interaction);
+  } catch (err) {
+    console.error(err);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    });
   }
+
+  // if (commandName === 'ping') {
+  //   await interaction.reply('Pong!');
+  // } else if (commandName === 'server') {
+  //   await interaction.reply(
+  //     `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+  //   );
+  // } else if (commandName === 'user') {
+  //   await interaction.reply(
+  //     `Your Tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
+  //   );
+  // }
 });
 
 // Login to Discord with your client's token
